6d4427ab24faf6bc84b47bb47355085aeba59b08
azalex020204 <azalexxx02@gmail.com>
2022-09-06 06:43:42 -0700

    details

--- js/btnShowMore.js
+++ js/btnShowMore.js
@@ -10,7 +10,7 @@ for (let i = 0; i < coll.length; i++) {
       content.style.display = block;
     }
   })
-}
+};
 let coll2 = document.getElementsByClassName('show__more');
 for (let i = 0; i < coll2.length; i++) {
   coll2[i].addEventListener('click', function () {
@@ -23,7 +23,7 @@ for (let i = 0; i < coll2.length; i++) {
       content.style.display = block;
     }
   })
-}
+};
 let coll3 = document.getElementsByClassName('showMore__productBtn');
 for (let i = 0; i < coll3.length; i++) {
   coll3[i].addEventListener('click', function () {
@@ -36,4 +36,17 @@ for (let i = 0; i < coll3.length; i++) {
       content.style.display = block;
     }
   })
-}
+};
+let coll4 = document.getElementsByClassName('hiddenBtnShow');
+for (let i = 0; i < coll4.length; i++) {
+  coll4[i].addEventListener('click', function () {
+    this.nextElementSibling.classList.toggle('active');
+    let content = this.nextElementSibling;
+    if (content.style.display) {
+      content.style.display = none;
+    }
+    else {
+      content.style.display = block;
+    }
+  })
+};
