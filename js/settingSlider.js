56f1bf10d39db4e4505e8ff7a0f676e10c89711f
azalex020204 <azalexxx02@gmail.com>
2022-09-11 01:17:48 -0700

    details

--- js/settingSlider.js
+++ js/settingSlider.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
   $('.smollImg').slick({
-    infinite: true,
     slidesToShow: 6,
     slidesToScroll: 1,
     asNavFor: '.bigSlider'
   });
   $('.bigSlider').slick({
-    infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     asNavFor: '.smollImg'
