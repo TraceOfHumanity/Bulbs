e69fd0dc290ba0521e03e7fa66e75cf20b491616
azalex020204 <azalexxx02@gmail.com>
2022-09-15 12:34:15 -0700

    detail

--- js/selectTypeBtn.js
+++ js/selectTypeBtn.js
@@ -1,5 +1,23 @@
-document.querySelectorAll('.type__btn').forEach(function (selectedBtn) {
-  selectedBtn.addEventListener('click', function (e){
-    selectedBtn.classList.toggle('selectedView')
+// const type = document.querySelectorAll('.type').forEach(function (selectedBtn){
+//   console.log(type);
+//   selectedBtn.querySelectorAll('.type__btn').forEach(element => {
+//   element.addEventListener('click', function(e){
+//     selectedBtn.forEach(nav=>nav.classList.remove('selectedView'))
+//     this.classList.add('selectedView');
+//   })
+// })
+// })
+
+const type = document.getElementById('type').querySelectorAll('.type__btn');
+console.log(type);
+type.forEach(element => {
+  element.addEventListener('click', function(){
+    type.forEach(nav=>nav.classList.remove('selectedView'))
+    this.classList.add('selectedView');
   })
 })
+// document.querySelectorAll('.type__btn').forEach(function (selectedBtn) {
+//   selectedBtn.addEventListener('click', function (e){
+//     selectedBtn.classList.toggle('selectedView')
+//   })
+// })
