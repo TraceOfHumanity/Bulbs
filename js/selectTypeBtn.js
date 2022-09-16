46b1a1d558776d6ea3ef3086ba975f91c147d647
azalex020204 <azalexxx02@gmail.com>
2022-09-15 20:54:29 -0700

    jQ

--- js/selectTypeBtn.js
+++ js/selectTypeBtn.js
@@ -1,23 +1,10 @@
-// const type = document.querySelectorAll('.type').forEach(function (selectedBtn){
-//   console.log(type);
-//   selectedBtn.querySelectorAll('.type__btn').forEach(element => {
-//   element.addEventListener('click', function(e){
-//     selectedBtn.forEach(nav=>nav.classList.remove('selectedView'))
-//     this.classList.add('selectedView');
-//   })
-// })
-// })
-
-const type = document.getElementById('type').querySelectorAll('.type__btn');
-console.log(type);
-type.forEach(element => {
-  element.addEventListener('click', function(){
-    type.forEach(nav=>nav.classList.remove('selectedView'))
-    this.classList.add('selectedView');
+document.querySelectorAll('.type').forEach(function (typeWrapper){
+  const typeBtn = typeWrapper.getElementsByTagName('DIV');
+  const arrayTypeBtn = Array.from(typeBtn);
+  arrayTypeBtn.forEach((el) => {
+  el.addEventListener('click', function(){
+    $(arrayTypeBtn).removeClass('selectedView')
+     this.classList.add('selectedView')
+  })
   })
 })
-// document.querySelectorAll('.type__btn').forEach(function (selectedBtn) {
-//   selectedBtn.addEventListener('click', function (e){
-//     selectedBtn.classList.toggle('selectedView')
-//   })
-// })
