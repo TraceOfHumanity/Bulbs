e3991feeb4aea1c5544a717b2038dacb09f27aec
azalex020204 <azalexxx02@gmail.com>
2022-09-17 11:06:47 -0700

    bg100%

--- js/darckBg.js
+++ js/darckBg.js
@@ -1,30 +1,3 @@
-// const Shadow = () => {
-//   if (document.querySelector('.dropdown').classList.contains('dropdown__list--visible')) {
-//     return $('.dropdown').css({ 'filter': 'drop-shadow(30px 10px 4px #4444dd)' })
-//   }
-// }
-
-// console.log(Shadow())
-
-//let dropDown = document.getElementsByClassName('dropdown');
-
-
-// document.contains('dropdown__list--visible')
-// ?  $(document.getElementsByClassName('dropdown')).css({ 'filter': 'drop-shadow(30px 10px 4px #4444dd)'})
-// : console.log(-1)
-
-// let dropDown = document.getElementsByClassName('dropdown');
-// for (let i = 0; i < dropDown.length; i++) {
-//   dropDown[i].addEventListener('click', function (e) {
-//     this.classList.add('shadowBg');
-
-//   })
-//   this.addEventListener('click', function (e) {
-//     if (e.target !== dropDown) {
-//       dropDown[i].classList.remove('shadowBg');
-//     }
-//   })
-// };
 let dropDown = document.getElementsByClassName('dropdown');
 let dropdownLists = document.getElementsByName('dropdown__lists');
 let dpordownItem = document.getElementsByName('dropdown__list-item');
@@ -39,6 +12,5 @@ for (let i = 0; i < dropDown.length; i++) {
     }
     
   })
-  // dropDown[i].classList.remove('shadowBg');
 };
 
